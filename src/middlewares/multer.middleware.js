35c9938e7f89c76fa5f434d69653aa6b7b5b0824
Amit chowdhury <camit8546@gmail.com>
2024-08-07 18:59:06 +0530

    all setup configured

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage=multer.diskStorage({
+    destination:function (req,file,cb) {
+        cb(null,"./public/temp")
+    },
+    filename:function (req,file,cb) {
+        const fileName=`${Date.now()}-${file.originalname}`;
+        cb(null,fileName);
+    }
+})
+
+const upload=multer({storage:storage});
+module.exports=upload;
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET
+})
+
+const uploadOnCloudinary=async (localFilePath)=>{
+   try {
+     if (!localFilePath) {
+        return null;
+     }
+     else{
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log('file uploaded successfully',response.url);
+        return response;
+     }
+   } catch (error) {
+     fs.unlinkSync(localFilePath);//remove locally stored file as the upload operation got failed
+     return null;
+   }
+}
