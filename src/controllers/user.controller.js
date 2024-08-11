52fe1863e3fd913fbfcf163b33f14e4f6e55e5af
Amit chowdhury <camit8546@gmail.com>
2024-08-11 22:05:21 +0530

    aggregation pipeline for userChannelProfile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -48,7 +48,6 @@ const registerUser=asyncHandler(async(req,res)=>{
    if (!avatarLocalPath) {
       throw new ApiError(400,"avatar is required");
    }
-
    const avatar=await uploadOnCloudinary(avatarLocalPath);
    const coverImage=await uploadOnCloudinary(coverImageLocalPath);
 
@@ -291,6 +290,79 @@ const updateCoverImage=asyncHandler(async(req,res)=>{
    .json(new ApiResponse(200,user,"coverImage updated successfully"));
 })
 
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+   const {username}=req.params;
+
+   if(!username?.trim()){
+    throw new ApiError(400,"username is required");
+   }
+
+   const channel=await User.aggregate([
+    {
+      $match:{
+        username:username?.toLowerCase()
+      }
+    },
+    {
+      $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"channel",
+        as:"subscribers"
+      }
+    },
+    {
+      $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"subscriber",
+        as:"subscribedTo"
+      }
+    },
+    {
+      $addFields:{
+        subscribersCount:{
+          $size:"subscribers"
+        },
+        channelsSubscribedTocount:{
+          $size:"subscribedTo"
+        },
+        isSubscribed:{
+          $cond:{
+            if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+            then:true,
+            else:false
+          }
+        }
+      }
+    },
+    {
+      $project:{
+        fullname:1,
+        email:1,
+        subscribersCount:1,
+        channelsSubscribedTocount:1,
+        isSubscribed:1,
+        avatar:1,
+        coverImage:1,
+      }
+    }
+   ])
+
+   if (!channel?.length) {
+    throw new ApiError(400,"channel does not exist");
+  }
+  
+  return res.status(200)
+  .json(
+    new ApiResponse(200, channel[0],"user channel fetched successfully")
+  );
+})
+ 
+const getWathHistory=asyncHandler(async(req,res)=>{
+  
+
+})
 
 export {
     registerUser,
@@ -301,5 +373,6 @@ export {
     getCurrentUser,
     updateDetails,
     updateAvatar,
-    updateCoverImage
+    updateCoverImage,
+    getUserChannelProfile
 }
