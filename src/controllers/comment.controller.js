f0d77e195bd2a880570abec74b08194847009917
Amit chowdhury <camit8546@gmail.com>
2024-08-22 15:03:59 +0530

    write deleteComment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -63,9 +63,34 @@ const updateComment=asyncHandler(async(req,res)=>{
 })
 
 
+const deleteComment=asyncHandler(async(req,res)=>{
+    const {commentId}=req.params;
+    if (!commentId) {
+        throw new ApiError(400,"comment id is required");
+    }
 
+    const comment=await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(400,"comment does not exist");
+    }
+    if (!comment.owner.equals(req.user._id)) {
+        throw new ApiError(400,"you are not authorized to delete comment");
+    }
+
+    try {
+        await Comment.findByIdAndDelete(commentId);
+    } catch (error) {
+        throw new ApiError(400,"error occured while deleting comment");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200,comment,"comment deleted successfully"));
+
+})
 
 export{
     addComment,
-    updateComment
+    updateComment,
+    deleteComment
 }
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment, updateComment } from "../controllers/comment.controller.js";
+import { addComment, deleteComment, updateComment } from "../controllers/comment.controller.js";
 
 const router=Router();
 
@@ -9,6 +9,7 @@ router.use(verifyJWT);
 
 router.post('/addComment/:videoId',addComment);
 router.patch('/updateComment/:commentId',updateComment);
+router.delete('/deleteComment/:commentId',deleteComment);
 
 
 export default router;
